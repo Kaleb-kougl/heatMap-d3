c50817111202e91d0c4638ba23cacdecb61b5fb7
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-07 13:19:36 -0600

    adding color filling function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let dataset;
 HTTP.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200) {
         dataset = JSON.parse(HTTP.responseText);
-        // console.log(dataset);
+        console.log(dataset);
         d3Commands();
     } else {
         console.log("something went wrong");
@@ -18,7 +18,7 @@ HTTP.onreadystatechange = function(){
 function d3Commands() {
   // console.log(dataset);
 
-  const PADDING = 20;
+  const PADDING = 55;
   const BAR_HEIGHT = 30;
   const BAR_WIDTH = 5;
   const WIDTH = PADDING + (Math.ceil(dataset['monthlyVariance'].length / 12) * BAR_WIDTH) + PADDING;
@@ -30,7 +30,13 @@ function d3Commands() {
     .domain([X_MIN, X_MAX])
     .range([PADDING, WIDTH-PADDING]);
   const X_AXIS = d3.axisBottom(X_SCALE).tickFormat(TIME_FORMAT);
-  let index = 0;
+  const Y_MIN = new Date(0, d3.min(dataset['monthlyVariance'], (d) => d['month'] - 1));
+  const Y_MAX = new Date(0, d3.max(dataset['monthlyVariance'], (d) => d['month'] - 1));
+  const MONTH_FORMAT = d3.timeFormat("%B");
+  const Y_SCALE = d3.scaleTime()
+    .domain([Y_MIN, Y_MAX])
+    .range([PADDING, HEIGHT-PADDING]);
+  const Y_AXIS = d3.axisLeft(Y_SCALE).tickFormat(MONTH_FORMAT);
 
   const svg = d3.select('body')
     .append('svg')
@@ -41,22 +47,48 @@ function d3Commands() {
     .data(dataset['monthlyVariance'])
     .enter()
     .append('rect')
-    .attr('x', (d, i) => {
-      // console.log(i % 12)
-      if (i % 12 === 0&& i !== 0) {
-        index++;
-      }
-      return (index * BAR_WIDTH) + PADDING
-    })
-    .attr('y', (d, i) => (i % 12) * BAR_HEIGHT)
+    .attr('x', (d, i) => X_SCALE(new Date(d['year'], 0)))
+    .attr('y', (d, i) => Y_SCALE(new Date(0, d['month'] - 1)))
     .attr('height', BAR_HEIGHT)
     .attr('width', BAR_WIDTH)
-    .style('fill', 'red')
-    .attr('stroke-width', 1.5)
-    .attr('stroke', 'black');
+    .style("margin-top", "0px")
+    .style('fill', (d, i) => determineColor(d));
 
   svg.append('g')
-    .attr('transform', 'translate(0,' + (HEIGHT - PADDING) + ')')
+    .attr('transform', 'translate(0,' + (HEIGHT - PADDING + BAR_HEIGHT) + ')')
     .attr('id', 'x-axis')
     .call(X_AXIS);
+
+  svg.append('g')
+    .attr('transform', 'translate(' + (PADDING) + ',' + (BAR_HEIGHT / 2) + ')')
+    .attr('id', 'y-axis')
+    .call(Y_AXIS);
+}
+
+function determineColor(data) {
+  if (data['variance'] <= -6) {
+    return 'rgb(50, 57, 147)';
+  } else if (data['variance'] <= -5 && data['variance'] > -6){
+    return 'rgb(71, 118, 178)';
+  } else if (data['variance'] <= -4 && data['variance'] > -5){
+    return 'rgb(118, 173, 207)';
+  } else if (data['variance'] <= -3 && data['variance'] > -4){
+    return 'rgb(172, 217, 232)';
+  } else if (data['variance'] <= -2 && data['variance'] > -3){
+    return 'rgb(224, 243, 248)';
+  } else if (data['variance'] <= -1 && data['variance'] > -2){
+    return 'rgb(255, 254, 194)';
+  } else if (data['variance'] <= 0 && data['variance'] > -1){
+    return 'rgb(253, 223, 149)';
+  } else if (data['variance'] <= 1 && data['variance'] > 0) {
+    return 'rgb(251, 173, 104)';
+  } else if (data['variance'] <= 2 && data['variance'] > 1) {
+    return 'rgb(242, 110, 74)';
+  } else if (data['variance'] <= 3 && data['variance'] > 2) {
+    return 'rgb(212, 50, 46)';
+  } else if (data['variance'] > 3){
+    return 'rgb(163, 6, 41)';
+  } else {
+    return 'rgb(255, 255, 255)';
+  }
 }
